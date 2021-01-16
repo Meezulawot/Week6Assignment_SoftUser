33926537cfaca2d471e847c97730a1c60297afa2
Meezu Lawot <omizuki123@gmail.com>
2021-01-16 20:22:14 +0545

    Fragments and bottom navigation activity (also added students in the list)

--- app/src/main/java/com/meezu/softuser/MainActivity.kt
+++ app/src/main/java/com/meezu/softuser/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.meezu.softuser
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.meezu.myapplication.model.Student
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnLogIn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etUsername = findViewById(R.id.etUsername)
+        etPassword = findViewById(R.id.etPassword)
+        btnLogIn = findViewById(R.id.btnLogIn)
+
+        btnLogIn.setOnClickListener{
+
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+
+            if(username == "softwarica" && password == "coventry"){
+                Toast.makeText(this, "LogIn successfull", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, BottomActivity::class.java)
+                startActivity(intent)
+
+            }else{
+                Toast.makeText(this, "Invalid username and password", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
